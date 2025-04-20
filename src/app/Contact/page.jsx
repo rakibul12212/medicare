c79cdac919a501cc9e9b6837496db809624be7e1
rakib12212 <rakibulislamr83@gmail.com>
2025-04-20 19:29:37 +0600

    breadcrumb

---
+++ src/app/Contact/page.jsx
@@ -0,0 +1,21 @@
+import Breadcrumb from "@/component/UI/Breadcrumb";
+import Container from "@/component/UI/Container/Container";
+import React from "react";
+
+const page = () => {
+  return (
+    <Container>
+      <div className="bg-gray-50">
+        <h1 className="text-4xl font-semibold text-left text-[#3B585E]  px-5 md:px-20 pt-10">
+          Contact
+        </h1>
+
+        <div className="px-5 md:px-20">
+          <Breadcrumb />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default page;
